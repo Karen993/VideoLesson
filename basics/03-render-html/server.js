c400c742e70646bfd948ac596c42d3a717fb876a
Karen993 <79135911+Karen993@users.noreply.github.com>
2021-02-16 16:45:14 +0400

    Create server.js

---
+++ basics/03-render-html/server.js
@@ -0,0 +1,9 @@
+let http = require('http');
+
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/plane'});
+    response.write('Hello world');
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
