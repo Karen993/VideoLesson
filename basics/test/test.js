bdf5fe671a44e614091093f7aa85209197b31634
Karen993 <79135911+Karen993@users.noreply.github.com>
2021-02-21 18:58:58 +0400

    2021.02.21

--- basics/test/test.js
+++ basics/test/test.js
@@ -1,6 +1,5 @@
-console.log(null == undefined)
-console.log(null == 0)
-console.log(Boolean(null))
-console.log(Boolean(undefined))
-let array = [1, 2, 3]
-console.log(array.join(""))
+for(let i=0; i<6; ++i){
+    setTimeout(()=>{
+        console.log('print');
+    },1000)
+}
